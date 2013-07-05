0af1eaaa8daed456c8f71f0d81dd0763949b57c5
Aditya Siram <aditya.siram@gmail.com>
2013-07-05 17:43:38 -0500

    More slides for intermediate states

--- CodeSnippets/StateExample.hs
+++ CodeSnippets/StateExample.hs
@@ -4,28 +4,16 @@ import Debug.Trace
 import Text.Printf
 
 println msg = trace msg (return ())
-minimum_bad :: [Int] -> ((), Int)
-minimum_bad [] = error "Empty List."
-minimum_bad xs =
-    runState (mapM_ compare xs :: State Int ()) (-1)
-    where
-      compare :: Int -> State Int ()
-      compare curr = do
-              old_min <- get
-              println (printf "old_min: %d curr: %d" old_min curr)
-              if (curr < old_min)
-              then put curr
-              else return ()
-
-minimum_good :: [Int] -> ((), Int)
-minimum_good [] = error "Empty List."
-minimum_good (x:xs) =
-    runState (mapM_ compare xs :: State Int ()) x
-    where
-      compare :: Int -> State Int ()
-      compare curr = do
-              old_min <- get
-              println ("old_min: " ++ (show old_min))
-              if (curr < old_min)
-              then put curr
-              else return ()
+minimum :: [Int] -> State Int ()
+minimum [] = error "Empty List."
+minimum xs =
+    forM_ xs (\curr -> do
+                old_min <- get
+                println (printf "old_min: %d curr: %d" old_min curr)
+                if (curr < old_min)
+                then put curr
+                else return ())
+-- main = let numbers = [3,2,1] in
+--        do print (runState (minimum numbers) (-1))
+main = let (n:ns) = [3,2,1] in
+       print (runState (Main.minimum ns) n)
